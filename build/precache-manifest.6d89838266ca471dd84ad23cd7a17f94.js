150a11a22f71435e124cb9729bcbf6b5aab3fb66
marvinkuang <marvinkuang@gmail.com>
2020-05-15 23:13:17 -0400

    last version

---
+++ build/precache-manifest.6d89838266ca471dd84ad23cd7a17f94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e206870e9fb511ad8c9201300ca92bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d23371b61eefdbb4731",
+    "url": "/static/css/main.29d7ca6e.chunk.css"
+  },
+  {
+    "revision": "cb80a15f71da0f888782",
+    "url": "/static/js/2.668f8c51.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.668f8c51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d23371b61eefdbb4731",
+    "url": "/static/js/main.fe57d6f9.chunk.js"
+  },
+  {
+    "revision": "6d8939353bdd9a2157b8",
+    "url": "/static/js/runtime-main.e11ed834.js"
+  }
+]);
